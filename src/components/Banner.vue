2463742c2ffd763df1a84e08342ea37a6861ca68
Andre Matos <60296220+AndreMTS@users.noreply.github.com>
2022-11-20 18:22:04 -0300

    #03- Estilização da home

---
+++ src/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="main-banner">
+        <h1>Monte seu Hamburguer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'buguer Banner',
+    
+}
+</script>
+
+<style scoped>
+#main-banner{
+    background-image: url('/img/burger.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    background-repeat: no-repeat;
+}
+#main-banner h1{
+    color: #fff;
+    text-align:center;
+    font-size: 60px;
+    padding:20px 40px;
+}
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+         <RouterLink to="/" id="logo-url">
+          <img v-bind:src="logo" v-bind:alt="alt" id="logo">
+         </RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/pedidos">Pedidos</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: "Navbar",
+  props:['logo','alt']
+};
+</script>
+
+<style scoped>
+nav{
+  background-color: #222;
+  border-bottom:4px solid #111;
+  padding:15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+nav #logo-url{
+  margin: auto;
+  margin-left: 0;
+}
+#logo{
+  width: 40%;
+  height: 40%;
+}
+nav a {
+  color:rgb(153, 87, 0);
+  text-decoration: none;
+  margin: 12px;
+  transition: .5s;
+}
+nav a:hover {
+  color:rgb(255, 255, 255);
+}
+</style>
